5b44b79a556ded75256654e06488e9f6f5d0e051
muammarzaki <muammarzaki2004@gmail.com>
2024-12-14 20:26:15 +0700

    first commit

---
+++ src/main/java/uin/Working.java
@@ -0,0 +1,48 @@
+package uin;
+
+import java.util.List;
+
+/**
+ * ini adalah class yang menjadi tempat kalian mengerjakan soal ujian kalian
+ */
+public class Working {
+    /**
+     * @return harus dapat mengembalikan nilai sebuah list 2 dimensi yang terdiri dari 10 entry/baris dan 2 colom
+     * dan tiap colom terdiri dari nama [string] dan pekerjaan [string].
+     */
+    public static List<List<String>> createAListsTest() {
+        return null;
+    }
+
+    /**
+     * anda harus dapat mengurutkan data yang di berikan, berdasarkan nama pekerjaan
+     *
+     * @param data data berbentuk seperti data cari @see createAListTest() 10 baris dan 2 column
+     * @return mengembalikan data yang sudah di urutkan
+     */
+    public static List<List<String>> sortTest(List<List<String>> data) {
+        return null;
+    }
+
+    /**
+     * anda harus dapat membalikkan sebuah nilai string
+     * contoh jawa menjadi awaj dan doleh menjadi helod
+     *
+     * @param data berupa string
+     * @return berupa nilai string yang sudah dibalik
+     */
+    public static String stackTest(String data) {
+        return null;
+    }
+
+    /**
+     * Selesaikan fungsi queue_test yang menerima list angka merepresentasikan waktu layanan pelanggan di
+     * dua kasir. Fungsi harus mengembalikan list (yang berukuran 2 [kasir1, kasir2]) waktu total layanan kedua kasir.
+     *
+     * @param data adalah list waktu tiap pelangan
+     * @return sebuah list berukuran 2 yang terdiri dari [kasir1, kasir2]
+     */
+    public static List<Integer> queueTest(List<Integer> data) {
+        return null;
+    }
+}
